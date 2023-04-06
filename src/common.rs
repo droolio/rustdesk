6f73909ab3155dbd73d15673d11e5b61428bfe2b
droolio <997518+droolio@users.noreply.github.com>
2023-04-06 03:18:08 +0100

    Hardcode own server and key

--- src/common.rs
+++ src/common.rs
@@ -583,7 +583,7 @@ pub fn check_software_update() {
 async fn check_software_update_() -> hbb_common::ResultType<()> {
     sleep(3.).await;
 
-    let rendezvous_server = format!("rs-sg.rustdesk.com:{}", config::RENDEZVOUS_PORT);
+    let rendezvous_server = format!("desk.certtech.co.uk:{}", config::RENDEZVOUS_PORT);
     let (mut socket, rendezvous_server) =
         socket_client::new_udp_for(&rendezvous_server, RENDEZVOUS_TIMEOUT).await?;
 
